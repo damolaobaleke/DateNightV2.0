e2ac353fa4ae5738247161f6fa641f540ab9b1d1
adolapo <abdulkareemdolapo@gmail.com>
2021-07-15 23:43:07 -0600

    Updating change password activity

--- app/src/main/java/com/ltd_immersia_datenight/views/datehub_navigation/ui_fragments/accounts/ChangePasswordActivity.kt
+++ app/src/main/java/com/ltd_immersia_datenight/views/datehub_navigation/ui_fragments/accounts/ChangePasswordActivity.kt
@@ -1,10 +1,12 @@
 package com.ltd_immersia_datenight.views.datehub_navigation.ui_fragments.accounts
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.ltd_immersia_datenight.R
@@ -12,7 +14,12 @@ import com.ltd_immersia_datenight.R
 class ChangePasswordActivity : AppCompatActivity() {
     private lateinit var oldPassword: EditText
     private lateinit var newPassword: EditText
+    private lateinit var confirmPassword: EditText
     private lateinit var changePasswordBtn: Button
+    private lateinit var oldPasswordTextInputLayout: TextInputLayout
+    private lateinit var newPasswordTextInputLayout: TextInputLayout
+    private lateinit var confirmPasswordTextInputLayout: TextInputLayout
+    val TAG = "ChangePasswordActivity"
     val user = FirebaseAuth.getInstance().currentUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +32,10 @@ class ChangePasswordActivity : AppCompatActivity() {
 
         oldPassword = findViewById(R.id.oldPasswordEditText)
         newPassword = findViewById(R.id.newPasswordEditText)
+        confirmPassword = findViewById(R.id.confirmPasswordEditText)
+        oldPasswordTextInputLayout = findViewById(R.id.oldPasswordTextInputLayout)
+        newPasswordTextInputLayout = findViewById(R.id.newPasswordTextInputLayout)
+        confirmPasswordTextInputLayout = findViewById(R.id.confirmPasswordTextInputLayout)
         changePasswordBtn = findViewById(R.id.changePasswordButton)
         changePasswordBtn.setOnClickListener {
             changePassword()
@@ -40,17 +51,21 @@ class ChangePasswordActivity : AppCompatActivity() {
     fun changePassword(){
         var oldPass = oldPassword.text.toString()
         var newPass = newPassword.text.toString()
-        var email = user!!.email
+        var confirmPass = confirmPassword.text.toString()
+        val email = user!!.email
 
         val credential = EmailAuthProvider.getCredential(email!!, oldPass)
         user.reauthenticate(credential)
                 .addOnFailureListener{
-                    val toast = Toast.makeText(this, "The old password you entered is incorrect", Toast.LENGTH_SHORT)
-                    toast.show()
+                    oldPasswordTextInputLayout.error = "Incorrect old password enter"
+                    //val toast = Toast.makeText(this, "The old password you entered is incorrect", Toast.LENGTH_SHORT)
+                   // toast.show()
 
                 }
                 .addOnSuccessListener {
-                    user.updatePassword(newPass)
+                    val error = validateNewPassword(newPass, confirmPass)
+                    if (error.isEmpty()){
+                        user.updatePassword(newPass)
                             .addOnCompleteListener { task ->
                                 if(task.isSuccessful){
                                     val toast = Toast.makeText(this, "Successfully updated password", Toast.LENGTH_SHORT)
@@ -58,8 +73,35 @@ class ChangePasswordActivity : AppCompatActivity() {
                                     onBackPressed()
                                 }
                             }
+                    } else {
+                        newPasswordTextInputLayout.error = error
+                        confirmPasswordTextInputLayout.error = error
+                    }
                 }
 
 
+    }
+
+    fun validateNewPassword(newPasswordValue: String, confirmPasswordValue: String): String{
+        var error = "";
+
+        val PASSWORD_PATTERN = ".*[a-z]+.*"
+        val PASSWORD_PATTERN_2 = ".*[A-Z]+.*"
+        val PASSWORD_PATTERN_3 = ".*[0-9]+.*"
+        val PASSWORD_PATTERN_4 = ".*[!@#$&*_\\\\-]+.*"
+        if (newPasswordValue.length < 8 ||
+            !(newPasswordValue.matches(Regex(PASSWORD_PATTERN)) && newPasswordValue.matches(Regex(PASSWORD_PATTERN_2)) &&
+                    newPasswordValue.matches(Regex(PASSWORD_PATTERN_3)) && newPasswordValue.matches(Regex(PASSWORD_PATTERN_4)))
+        ) {
+            Log.e(TAG, newPasswordValue.matches(Regex(PASSWORD_PATTERN)).toString())
+            Log.e(TAG, newPasswordValue.matches(Regex(PASSWORD_PATTERN_2)).toString())
+            Log.e(TAG, newPasswordValue.matches(Regex(PASSWORD_PATTERN_3)).toString())
+            Log.e(TAG, newPasswordValue.matches(Regex(PASSWORD_PATTERN_4)).toString())
+            error  =
+                "Password must be a minimum of 8 characters and must contain uppercase, lowercase, number and one of !, @, #, $, &, *, _, \\, -"
+        } else if (newPasswordValue != confirmPasswordValue){
+            error = "Passwords entered do not match"
+        }
+        return error;
     }
 }
--- app/src/main/java/com/ltd_immersia_datenight/views/datehub_navigation/ui_fragments/dates/pending/PendingFragment.java
+++ app/src/main/java/com/ltd_immersia_datenight/views/datehub_navigation/ui_fragments/dates/pending/PendingFragment.java
@@ -379,7 +379,7 @@ public class PendingFragment extends Fragment implements DatePickerDialog.OnDate
     private void pickTime() {
         TimePickerDialog timeDialog = new TimePickerDialog(requireContext(), this,
                                                            Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
-                                                           Calendar.getInstance().get(Calendar.MINUTE), true);
+                                                           Calendar.getInstance().get(Calendar.MINUTE), false);
         timeDialog.show();
     }
 
@@ -423,17 +423,24 @@ public class PendingFragment extends Fragment implements DatePickerDialog.OnDate
 
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        String AM_PM="";
+        int hour12HourClock;
+        String AM_PM = "";
 
-        Calendar calendar = Calendar.getInstance();
-        if(calendar.get(Calendar.AM_PM) == Calendar.AM){
-            AM_PM = "am";
-        }else if(calendar.get(Calendar.AM_PM) == Calendar.PM){
-            AM_PM = "pm";
-        }else{
-            AM_PM = "";
+        if (hourOfDay == 0){ // 12.00 AM
+            AM_PM = "AM";
+            hour12HourClock = 12;
         }
-        timeChosen.setText(String.format(Locale.getDefault(), "%01d:%02d %s", hourOfDay, minute, AM_PM));
+        else if (hourOfDay < 12){
+            AM_PM = "AM";
+            hour12HourClock = hourOfDay;
+        }  else if (hourOfDay == 12){ // 12:00 PM
+            AM_PM = "PM";
+            hour12HourClock = hourOfDay;
+        } else {
+            AM_PM = "PM";
+            hour12HourClock = hourOfDay - 12;
+        }
+        timeChosen.setText(String.format(Locale.getDefault(), "%01d:%02d %s", hour12HourClock, minute, AM_PM));
     }
 
     public static Timestamp dateStringToTimestamp(String dateStr) {
